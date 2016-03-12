d00e80336589e8549af787ad65a0577c3ad52d4d
ThomasJohnson <tjohnson1106@gmail.com>
2016-03-12 12:20:25 -0500

    added forms and handleSubmit method to app component

--- App.jsx
+++ App.jsx
@@ -17,12 +17,34 @@ App = React.createClass({
       return <Task key={task._id} task={task} />;
     });
   },
+
+   handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+ 
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
  
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
  
         <ul>
